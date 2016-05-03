abce6a416b98f88f0d8cb24e1d7dd5a2c83ed83d
paulopez78 <paulopez78@gmail.com>
2016-05-03 12:53:56 +0200

    deply and scale

--- swarm/deploy.sh
+++ swarm/deploy.sh
@@ -1,6 +1,10 @@
+eval $(docker-machine env manager)
 DOCKER_HOST=$(docker-machine ip manager):3376
 
 cd ..
-docker-compose restart
-docker-compose build
-docker-compose up
+docker-compose up --build --force-recreate -d
+docker-compose scale quiz-ui=5 quiz-api=5
+docker-compose logs
+
+cd swarm
+./restart.sh
