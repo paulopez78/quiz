90d0cbaa8fa7a688763114237885ba59dbbb67d4
paulopez78 <pau.lopez.78@gmail.com>
2016-05-09 13:30:17 +0200

    refactoring

--- quiz-ui/src/containers/common.js
+++ quiz-ui/src/containers/common.js
@@ -1,20 +1,8 @@
 export function selectedQuestion(questionId, questions){
-  const {
-    answerVisible,
-    resultVisible,
-    selectedOption,
-    options
-  } = questions.find((x) => x.id === questionId) || {
+  return questions.find((x) => x.id === questionId) || {
     answerVisible: false,
     resultVisible: false,
     selectedOption:-1,
     options:[]
-  }
-
-  return {
-    answerVisible,
-    resultVisible,
-    selectedOption,
-    options
   }
 }
