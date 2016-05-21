37bad84a4818ac03366f01f1feea7819f989f910
paulopez78 <pau.lopez.78@gmail.com>
2016-05-21 20:58:36 +0200

    test script with parameters

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@ docker rm -f load_test
 
 docker run \
 -it \
--e=QUIZ_API_HOST=192.168.99.100 \
--e=QUIZ_API_PORT:3000 \
+-e=QUIZ_API_HOST=$1 \
+-e=QUIZ_API_PORT=$2 \
 --name load_test \
---entrypoint="python test.py" \
-paulopez/quiz-worker
+--entrypoint="python" \
+paulopez/quiz-worker "test.py"
